5653ac945e979d73fcb0c4665312beebb5b5e299
DJHPN222 <154042135+DJHPN222@users.noreply.github.com>
2023-12-17 14:01:52 +0800

    Add files via upload

---
+++ vue.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  pluginOptions: {
+    i18n: {
+      locale: 'zh',
+      fallbackLocale: 'en',
+      localeDir: 'locales',
+      enableInSFC: true
+    }
+  },
+
+  lintOnSave: undefined,
+  publicPath: undefined,
+  outputDir: undefined,
+  assetsDir: undefined,
+  runtimeCompiler: undefined,
+  productionSourceMap: false,
+  parallel: undefined,
+  css: undefined
+}
